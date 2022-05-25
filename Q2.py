ec8158c05dd79f5493ed449111e02e45c60aca95
Pavan Kumar <56595590+pavankmr1@users.noreply.github.com>
2022-05-25 11:34:25 +0530

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,14 +1,16 @@
-class Solution:
+class Solution(object):
+    
     def calPascal(self,i,j):
-        if  j==0 or j==1:
+        if  j==0 or j==i:
             return 1
         else:
-                return calPascal(i-1,j-1)+calPascal(i-1,j)
-    def generate(self,numRows) ->list[list[int]]:
+            return self.calPascal(i-1,j-1)+self.calPascal(i-1,j)
+    
+    def generate(self,numRows: int)-> list:
         triangle =[]
         for i in range(numRows):
             add=[]
-            for j in range(numRows+1):
+            for j in range(i+1):
                 add.append(self.calPascal(i,j))
             triangle.append(add)
         return triangle
