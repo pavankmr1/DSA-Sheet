655e0319099463dfc4514c35a5dcf7a19a770095
Pavan Kumar <56595590+pavankmr1@users.noreply.github.com>
2022-06-01 23:25:01 +0530

    Create mergeSort.py

---
+++ mergeSort.py
@@ -0,0 +1,10 @@
+# MergeSort in Python
+
+
+def mergeSort(array):
+    if len(array) > 1:
+
+        #  r is the point where the array is divided into two subarrays
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
