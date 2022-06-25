94f6583550676bb4773c2d6acf506d7467ef0c69
pavankmr1 <pavankmr4501@gmail.com>
2022-06-25 10:33:35 +0530

    file uploaded

---
+++ Day4_Arrays/Q3(1).py
@@ -0,0 +1,21 @@
+class Solution:
+    def longest_Streak(self,nums):
+        if not nums: return 0
+
+        nums.sort()
+
+        longest_Streak =1
+        current_streak = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                if nums[i] == nums[i-1]+1:
+                    current_streak += 1
+                else:
+                    longest_Streak = max(current_streak,longest_Streak)
+                    current_streak = 1
+
+        return max(current_streak,longest_Streak)
+
+a = Solution()
+print(a.longestConsecutive([100,4,200,1,3,2]))
