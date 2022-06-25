3326539d3acbfb908753e5043e0e342af58bf89f
pavankmr1 <pavankmr4501@gmail.com>
2022-06-25 12:34:21 +0530

    file uploaded

--- Day4_Arrays/Q3(2).py
+++ Day4_Arrays/Q3(2).py
@@ -19,4 +19,10 @@ class Solution:
 a = Solution()
 print(a.longest_Streak([100,4,200,1,3,2]))
 
+# Time complexity : O(n).
 
+# Although the time complexity appears to be quadratic due to the while loop nested within the for loop,
+#  closer inspection reveals it to be linear. Because the while loop is reached only when currentNum marks the beginning of a sequence 
+# (i.e. currentNum-1 is not present in nums), the while loop can only run for nn iterations throughout the entire runtime of the algorithm. 
+# This means that despite looking like O(n \cdot n)O(n⋅n) complexity, the nested loops actually run in O(n + n) = O(n)O(n+n)=O(n) time.
+#  All other computations occur in constant time, so the overall runtime is linear.
