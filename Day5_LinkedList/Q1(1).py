b686186fe28a71ba84157a6ef7043a72aac92eb7
pavankmr1 <pavankmr4501@gmail.com>
2022-06-29 11:12:46 +0530

    file uploaded

---
+++ Day5_LinkedList/Q1(1).py
@@ -0,0 +1,73 @@
+# Simple and tail recursive Python program to
+# reverse a linked list
+ 
+# Node class
+class Node:
+
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+class LinkedList:
+
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+    
+    def reverseUtil(self,curr,prev):
+
+        # If last node mark it head
+        if curr.next is None:
+            self.head = curr
+
+            # Update next to prev node
+            curr.next = prev
+            return
+    
+        # save curr.next node for recursive call
+        next = curr.next
+
+        # update next
+        curr.next = prev
+
+        self.reverseUtil(next,curr)
+
+    #This function mainly calls reverseUti()
+    def reverse(self):
+        if self.head is None:
+            return
+        self.reverseUtil(self.head,None)
+
+    # Function to insert a new node at the beginning
+    def push(self,new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Utility function to print the Linked list
+    def printList(self):
+        temp = self.head
+        while temp:
+            print(temp.data)
+            temp = temp.next
+
+# Driver code
+llist = LinkedList()
+llist.push(8)
+llist.push(7)
+llist.push(6)
+llist.push(5)
+llist.push(4)
+llist.push(3)
+llist.push(2)
+llist.push(1)
+
+print("Given Linked List")
+llist.printList()
+
+llist.reverse()
+
+print("\n Reverse Linked List")
+llist.printList()
+
+
---
+++ Day5_LinkedList/README.md
@@ -0,0 +1,2 @@
+DAY-4 Linked List
+    Q1       => Reverse Linke list (Iterativ Method)(Time Complexity = O(N), Space Complexity = O(1))
