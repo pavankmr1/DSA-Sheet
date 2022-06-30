116fedb76872521eb2d232393dd82fac2d41287b
pavankmr1 <pavankmr4501@gmail.com>
2022-06-30 11:24:15 +0530

    file uploaded

---
+++ Day5_LinkedList/Q3.py
@@ -0,0 +1,56 @@
+import sys
+from sys import stdin
+sys.setrecursionlimit(10**6)
+
+class Node():
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+def sortTwoLists(first,second):
+
+    if first == None:
+        return second
+    
+    if second == None:
+        return first
+
+    if first.data < second.data:
+        first.next = sortTwoLists(first.next,second)
+        return first
+    else:
+        second.next = sortTwoLists(second.next,first)
+        return second
+
+def ll(arr):
+
+    if len(arr) == 0:
+        return None
+    head = Node(arr[0])
+    last = head
+
+    for data in arr[1:]:
+        last.next = Node(data)
+        last = last.next
+    return head
+
+def printll(head):
+
+    while head:
+
+        print(head.data, end =" ")
+        head = head.next
+
+
+arr1 = list(map(int, sys.stdin.readline().strip().split(" ")))
+arr2 = list(map(int, sys.stdin.readline().strip().split(" ")))
+# arr1 = [1,2,3,4,5]
+# arr2 = [6,7,8,9,10]
+
+l1 = ll(arr1[:])
+l2 = ll(arr2[:])
+
+
+l = sortTwoLists(l1, l2)
+
+printll(l)
