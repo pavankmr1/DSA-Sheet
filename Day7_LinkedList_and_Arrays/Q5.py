a63309d217b6bce0a5c7a427d3ec44c4f281be99
pavankmr1 <pavankmr4501@gmail.com>
2022-08-10 19:30:17 +0530

    file uploaded

---
+++ Day7_LinkedList_and_Arrays/Q5.py
@@ -0,0 +1,16 @@
+def removeDuplicates(arr,n):
+
+    # First pointer
+    i =0
+
+    # Second pointer traversing from 1 to n
+    for j in range (1,n):
+        # If not duplicates increment first pointer and check again
+        if (arr[j != arr[j-1]]):
+            arr[i] = arr[j]
+            i+=1
+            
+    
+    # Return length == (index of first pointer when second reaches end) +1
+    return i+1
+    
